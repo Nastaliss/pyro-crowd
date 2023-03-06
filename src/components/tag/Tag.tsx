e66b0533b09175966c7d0a767117ceecc909d0b1
Stanislas Bruhiere <stanbill91@gmail.com>
2023-03-07 00:54:00 +0100

    Prevent dragging images

--- src/components/tag/Tag.tsx
+++ src/components/tag/Tag.tsx
@@ -5,8 +5,8 @@ import { faCircleCheck } from '@fortawesome/free-solid-svg-icons'
 
 export const Tag = ({ tagInfo, enabled, onClick, tagId }: { tagInfo: TagInfo, enabled: boolean, onClick: (key: AllTags) => void, tagId: AllTags }): JSX.Element => {
   return (
-    <div className={`tagContainer ${enabled ? 'enabled' : 'disabled'}`}>
-      <div className='tag' onClick={() => onClick(tagId)}>
+    <div className={`tagContainer ${enabled ? 'enabled' : 'disabled'}`} onClick={() => onClick(tagId)}>
+      <div className='tag'>
         <img src={tagInfo.image}/>
       </div>
       <span>{enabled ? <FontAwesomeIcon icon={faCircleCheck} className='enabledIcon'/> : <></> }{tagInfo.title}</span>
